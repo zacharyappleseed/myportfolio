8af59fd30a1621afdc4cfd62aefcd8f400b55b74
zacharyappleseed <121331218+zacharyappleseed@users.noreply.github.com>
2023-03-11 06:58:11 -0500

    reverted changes. but whats left is another arrangements index. html page that can be customized.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import TopBarNav from './components/TopBarNav'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <TopBarNav />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
